4b649d3d6446955ee58085630fbf7cd3569c4665
Kri5t0f (K. Kordal) <53398204+Kri5t0fK@users.noreply.github.com>
2024-11-29 18:38:34 +0100

    Fix trailing zeros in MSE printing

--- main.py
+++ main.py
@@ -93,8 +93,8 @@ if __name__ == '__main__':
     mse_5_mean  = np.mean([mse_5_roll, mse_5_pitch, mse_5_yaw])
     # Porównanie MSE
     print("Sample count |  Roll MSE | Pitch MSE |  Yaw MSE  | Combined MSE")
-    print(f"     7000    | {mse_7_roll:9.6} | {mse_7_pitch:9.6} | {mse_7_yaw:9.6} | {mse_7_mean:.6}")
-    print(f"     5000    | {mse_5_roll:9.6} | {mse_5_pitch:9.6} | {mse_5_yaw:9.6} | {mse_5_mean:.6}")
+    print(f"     7000    | {mse_7_roll:9.6f} | {mse_7_pitch:9.6f} | {mse_7_yaw:9.6f} | {mse_7_mean:.6f}")
+    print(f"     5000    | {mse_5_roll:9.6f} | {mse_5_pitch:9.6f} | {mse_5_yaw:9.6f} | {mse_5_mean:.6f}")
 
     # Pierwsze 7000 - nałożone prawdziwe pozycja z [train part]
     fig, axs = plt.subplots(3, 1, figsize=(5, 5))
